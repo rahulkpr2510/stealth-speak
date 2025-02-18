13b2de2b52fc20cfa304ef7b2e775af7b3695027
rahulkapoor2005 <rahulkpr1972@gmail.com>
2025-02-19 01:22:47 +0530

    Fix formatting in authentication options file; separate import statements for clarity

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -2,8 +2,8 @@ import { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import bcrypt from 'bcryptjs';
 import dbConnect from '@/lib/dbConnect';
-import UserModel from '@/models/user.model';import { any } from 'zod';
-'
+import UserModel from '@/models/user.model';
+
 
 export const authOptions: NextAuthOptions = {
   providers: [
