36f451b44ad78a3daac3524347387af8110d6ae7
rahulkapoor2005 <rahulkpr1972@gmail.com>
2025-02-19 01:43:10 +0530

    Enhance TypeScript configuration and improve error handling in message deletion API

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -5,61 +5,70 @@ import UserModel from "@/models/user.model";
 import { AxiosError } from "axios";
 import { ApiResponse } from "@/types/ApiResponse";
 
-export async function DELETE(request: Request, {params}: {params: {messageid: string}}) {
-    const messageId = params.messageid.toString()
-    await dbConnect()
-    const session = await getServerSession(authOptions)
-    const user: User = session?.user as User
+interface Params {
+  params: {
+    messageid: string;
+  };
+}
 
-    if(!session || !user){
-        return Response.json(
-            {
-                success: false,
-                message: "User not authenticated !!"
-            },
-            {
-                status: 401
-            }
-        )
-    }
-    try {
-        const updatedResult = await UserModel.updateOne(
-            {_id: user._id},
-            {$pull: {messages: {_id: messageId}}}
-        )
+export async function DELETE(request: Request, context: Params) {
+  const { messageid } = context.params;
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
 
-        if(updatedResult.modifiedCount === 0){
-            return Response.json(
-                {
-                    success: false,
-                    message: "Message not found or already deleted !!"
-                },
-                {
-                    status: 404
-                }
-            )
+  if (!session || !user) {
+    return Response.json(
+      {
+        success: false,
+        message: "User not authenticated !!",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  try {
+    const updatedResult = await UserModel.updateOne(
+      { _id: user._id },
+      { $pull: { messages: { _id: messageid } } }
+    );
+
+    if (updatedResult.modifiedCount === 0) {
+      return Response.json(
+        {
+          success: false,
+          message: "Message not found or already deleted !!",
+        },
+        {
+          status: 404,
         }
-
-        return Response.json(
-            {
-                success: true,
-                message: "Message deleted successfully !!"
-            },
-            {
-                status: 200
-            }
-        )
-    } catch (error) {
-        const axiosError = error as AxiosError<ApiResponse>
-        console.error("Error in deleting the message", error)
-        return Response.json(
-            {
-                success: false,
-                message: axiosError.response?.data.message || "Something went wrong while deleting the message !!"
-            },
-            {
-                status: 500
-            }
-        )
+      );
     }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Message deleted successfully !!",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    const axiosError = error as AxiosError<ApiResponse>;
+    console.error("Error in deleting the message", error);
+    return Response.json(
+      {
+        success: false,
+        message:
+          axiosError.response?.data.message ||
+          "Something went wrong while deleting the message !!",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
